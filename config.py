6c4d82e1a2e4b8edd1f01a9855cfd3c973978f6f
wolf <arthurwolf.dev07@gmail.com>
2024-07-11 16:11:43 -0600

    UPDATE file config update for ais

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 import json
 from dotenv import load_dotenv
 
+
 # CARGAR VARIABLES DE ENTORNO
 load_dotenv()
 MAPON_BASE_URL = os.getenv('MAPON_BASE_URL')
@@ -10,9 +11,11 @@ CRM_LOGIN_URL = os.getenv('CRM_LOGIN_URL')
 COOKIES_SESSION_CRM = os.getenv("COOKIES_SESSION_CRM")
 
 
+# SALVAR LAS COOKIES EN DURO
 def save_crm_cookies(cookies):
     cookies_json = json.dumps(cookies)
     with open('.env', 'a') as file:
         set_cookies = f"COOKIES_SESSION_CRM={cookies_json}\n"
         file.write(set_cookies)
     return set_cookies
+
