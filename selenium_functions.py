f4624e1f3e90db59fd84801b39ac61efdd598814
wolf <arthurwolf.dev07@gmail.com>
2024-07-11 16:16:12 -0600

    CREATE selenium_functions file

--- selenium_functions.py
+++ selenium_functions.py
@@ -1,12 +1,15 @@
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+from config import COOKIES_SESSION_CRM
+
 
 def login_crm(url_target):
-    # CONFIGURAR EL NAVEGADOR
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     # ABRIR LA PAGINA DE INICIO DE SESIÓN
     driver.get(url_target)
@@ -19,3 +22,15 @@ def login_crm(url_target):
     # CERRAR EL NAVEGADOR
     driver.quit()
     return cookies
+
+
+def get_data_table(url_target):
+    # SETTEAR LAS COOKIES
+    chrome_options = Options()
+    cookies_data = json.loads(COOKIES_SESSION_CRM)
+    for cookie in cookies_data:
+        chrome_options.add_cookie(cookie)
+    # INIT chrome web driver
+    driver = webdriver.Chrome(options=chrome_options)
+    # ABRIR LA PAGINA
+    driver.get(url_target)
