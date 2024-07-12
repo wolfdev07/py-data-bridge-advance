bfadd4ce0e447c2e4b9c7f43cf14ef79932d0e52
wolf <arthurwolf.dev07@gmail.com>
2024-07-12 13:49:58 -0600

    UPDATE new frontend styles and functions

---
+++ static/js/index/loadNewCRM.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const newcrmButton =  document.getElementById("new-crm");
+
+newcrmButton.addEventListener("click", ()=>{
+    // EJECUTAR ANIMACION DE CARGA
+    LoadingPage();
+    // REDIRECCIONAR AL FORMULARIO
+    window.location.href="/cookies";
+});
